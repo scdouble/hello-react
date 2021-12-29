37aa818af476a39f14fefeafa3fb929b04016aac
Your Name <you@example.com>
2021-12-29 20:21:18 +0900

    start Hello and welcome react cli

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// React をImport
+import React, { Component } from 'react'
+import Hello from "./components/Hello/Hello"
+import Welcome from './components/Welcome/Welcome'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 殻のAppコンポーネントを作成してエクスポート
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+        <Welcome />
+      </div>
+
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Hello.css'
+
+export default class Hello extends React.Component {
+  render(){
+    return (
+      <h2 className='title'>Hello React!</h2>
+    )
+  }
+}
