d48991212b454e568a94ac6a1b57489e5a607b9d
Your Name <you@example.com>
2021-12-30 23:09:08 +0900

    create github search static page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import Search from './components/Search'
+import List from './components/List'
+
 export default class App extends Component {
-  getStudentData = (params) => {
-    axios.get('http://localhost:3000/api1/students').then(
-      response => { console.log('Success', response.data) },
-      error => { console.log('Fail', error) }
-    )
-  }
-
-  getCarData = (params) => {
-    axios.get('http://localhost:3000/api2/cars').then(
-      response => { console.log('Success', response.data) },
-      error => { console.log('Fail', error) }
-    )
-  }
-
   render() {
     return (
-      <div>
-        <button onClick={this.getStudentData}>Click me </button>
+      <div className="container">
+        <Search />
+        <List />
       </div>
     )
   }
