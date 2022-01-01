c2bc21d511d88036b4f9bcefc3920cd5f448b440
Your Name <you@example.com>
2022-01-01 21:02:03 +0900

    complete todolist pubsub

--- 05_src_github_user_search_axios/components/Search/index.jsx
+++ 05_src_github_user_search_axios/components/Search/index.jsx
@@ -1,34 +1,27 @@
 import React, { Component } from 'react'
-import PubSub from 'pubsub-js'
 import axios from 'axios'
 
 export default class index extends Component {
   search = (params) => {
-
-
-
     // ユーザInputを取得
     // 连续解构赋值 + 重命名
     const { keyWordElement: { value: keyWord } } = this
     console.log(keyWord);
 
     //Httpリクエストする前にStateを変更、
-    /* this.props.updateAppState({ isFirst: false, isLoading: true }) */
-    PubSub.publish('atguigu', { isFirst: false, isLoading: true })
+    this.props.updateAppState({ isFirst: false, isLoading: true })
+
     // リクエストを出す
     axios.get(`/api1/search/users?q=${keyWord}`).then(
       response => {
         console.log(response.data.items);
         //Httpリクエストする前にStateを変更、
-        /* this.props.updateAppState({ isLoading: false, users: response.data.items }) */
-        PubSub.publish('atguigu', { isLoading: false, users: response.data.items })
-
-        //  this.props.saveUsers(response.data.items)
+        this.props.updateAppState({ isLoading: false, users: response.data.items })
+      //  this.props.saveUsers(response.data.items)
       },
       error => {
         console.log(error)
-        // this.props.updateAppState({ isLoading: false, err: error.message })
-        PubSub.publish('atguigu', { isLoading: false, err: error.message })
+        this.props.updateAppState({ isLoading: false, err: error.message })
       }
     )
   }
