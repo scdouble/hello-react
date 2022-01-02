5aa95d23c98b932a89fedb9ddc5280ea76d29d51
Your Name <you@example.com>
2022-01-02 21:29:07 +0900

    how to use antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,32 @@
 import React, { Component } from 'react'
-import { Route, Switch,Redirect } from 'react-router-dom'
-import About from './pages/About'// ルーターのコンポーネント
-import Home from './pages/Home'
-import Header from './components/Header' // 一般のコンポーネント
-import MyNavLink from './components/MyNavLink'
+
+import { WechatOutlined } from '@ant-design/icons'
+import { Button, DatePicker } from 'antd';
+const { RangePicker } = DatePicker;
+
+import 'antd/dist/antd.css'
+
 
 export default class App extends Component {
 
+  onChange1 = (date, dateString) => {
+    console.log(date, dateString);
+  }
+
+  onChange2 = (dates, dateStrings) => {
+    console.log(dates, dateStrings);
+  }
   render() {
     return (
       <div>
-        <div className="row">
-          <div className="col-xs-offset-2 col-xs-8">
-            <Header />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-xs-2 col-xs-offset-2">
-            <div className="list-group">
-              {/* ネイティブHTMLは<a>を使ってページを遷移する */}
-              {/* <a className="list-group-item" href="./about.html">About</a>
-              <a className="list-group-item active" href="./home.html">Home</a> */}
-
-              {/* Reactの中でRouterを使ってComponentを切り替える Routeを編集する*/}
-              <MyNavLink to="/about">About</MyNavLink>
-              <MyNavLink to="/home">Home</MyNavLink>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="panel">
-              <div className="panel-body">
-                {/* Routeを登録 */}
-                <Switch>
-                  <Route path="/about" component={About} />
-                  <Route path="/home" component={Home} />
-                  <Redirect to="/about"/>
-                </Switch>
-
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div >
+        App...
+        <button>Click me</button>
+        <Button type="primary">Primary Button</Button>
+        <Button type="ghost">Primary Button</Button>
+        <WechatOutlined />
+        <DatePicker onChange={this.onChange1} />
+        <RangePicker onChange={this.onChange2} />
+      </div>
     )
   }
 }
