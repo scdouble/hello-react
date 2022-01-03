eee74b71418c937802924cc2acd727ffd8ec9d29
Your Name <you@example.com>
2022-01-03 14:48:55 +0900

    antd_and_antd_advance

---
+++ 22_src_antd_Advanced/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import { WechatOutlined } from '@ant-design/icons'
+import { Button, DatePicker } from 'antd';
+const { RangePicker } = DatePicker;
+
+// import 'antd/dist/antd.css'
+
+
+export default class App extends Component {
+
+  onChange1 = (date, dateString) => {
+    console.log(date, dateString);
+  }
+
+  onChange2 = (dates, dateStrings) => {
+    console.log(dates, dateStrings);
+  }
+  render() {
+    return (
+      <div>
+        App...
+        <button>Click me</button>
+        <Button type="primary">Primary Button</Button>
+        <Button type="ghost">Primary Button</Button>
+        <WechatOutlined />
+        <DatePicker onChange={this.onChange1} />
+        <RangePicker onChange={this.onChange2} />
+      </div>
+    )
+  }
+}
